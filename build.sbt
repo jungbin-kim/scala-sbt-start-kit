404ecd651f859d9e8926067da003140fd814bf64
Jungbin Kim <bin88829@letsee.kr>
2018-06-05 17:31:26 +0900

    Add mariaDB connection by slick 3.2.3

--- build.sbt
+++ build.sbt
@@ -4,6 +4,13 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies ++= Seq(
 
+val slickVersion = "3.2.3"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.slick" %% "slick" % slickVersion,
+  "com.typesafe.slick" %% "slick-hikaricp" % slickVersion,
+  "org.mariadb.jdbc" % "mariadb-java-client" % "1.5.9",
+  "com.typesafe" % "config" % "1.3.3",
+  "org.slf4j" % "slf4j-nop" % "1.6.4" // log library
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,43 @@
-object Main {
-  def main(args: Array[String]) = println("Hi!")
+
+import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Main extends App{
+  // Main thread
+  val mainThreadPromise = scala.concurrent.Promise[Int]()
+  val promiseFuture = mainThreadPromise.future
+
+  // DB config
+  val db = Database.forConfig("mydb")
+
+  val q = test.findByName("맥스")
+  val qResult = q.result
+
+  // query 문 확인
+  println(qResult.statements)
+
+  db.run(qResult) map (s => {
+    // Print query result
+    println(s)
+    // main thread 종료
+    mainThreadPromise.success(1)
+    s
+  })
+
+  // Main thread promise 가 success 1을 반환하면 종료
+  Await.result(promiseFuture, Duration.Inf)
+}
+
+object test extends TableQuery(new TestTable(_)) {
+  val findByName = this.findBy(_.name)
+}
+
+import com.typesafe.config.ConfigFactory
+class TestTable(tag: Tag) extends Table[(Int, String)](tag, Some(ConfigFactory.load().getString("mydb.custom.targetSchema")), ConfigFactory.load().getString("mydb.custom.testTableName")) {
+  def id = column[Int]("id", O.PrimaryKey)
+  def name = column[String]("name_ko")
+  def * = (id, name)
 }
